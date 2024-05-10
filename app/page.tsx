b8d3beb25e2ec3e39058fccff33624988c6b2e24
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2024-05-10 09:08:27 +0100

    👌Update: auto deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
 import { cn } from "@/lib";
-import { GeneralSans_SemiBold, GeneralSans_Meduim } from "@/lib/font";
+import { GeneralSans_SemiBold, GeneralSans_Meduim, GeneralSans_Regular } from "@/lib/font";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 export default function Page() {
 	return (
-		<div className="flex items-center justify-center h-screen">
-			<div>
-				<h1 className={cn("text-5xl text-center", GeneralSans_Meduim.className)}>Save much more on deployment bills</h1>
+		<>
+			<div className="lg:inline-grid m-auto lg:items-center lg:text-center lg:justify-center w-full pt-28">
+				<h1 className={cn("lg:text-7xl lg:w-[60%] m-auto lg:leading-[4.8rem]", GeneralSans_Meduim.className)}>Save much more on deployment &nbsp;bills</h1>
+				<p className={cn("py-8 text-xl lg:w-[50%] m-auto", GeneralSans_Regular.className)}>
+					A cloud deployment server developed by{" "}
+					<Link
+						href="https://curlback.com"
+						target="_blank">
+						<u>curlback</u>&nbsp;
+						<SquareArrowOutUpRight className="inline-flex w-4" />
+					</Link>{" "}
+					for{" "}
+					<Link href="https://hynitr.com">
+						<u>hynitr</u>&nbsp;
+						<SquareArrowOutUpRight className="inline-flex w-4" />
+					</Link>{" "}
+					projects. Powered by{" "}
+					<Link href="https://usedothost.com">
+						<u>dothost</u>&nbsp;
+						<SquareArrowOutUpRight className="inline-flex w-4" />
+					</Link>{" "}
+					and{" "}
+					<Link href="https://coolify.com">
+						<u>coolify</u>&nbsp;
+						<SquareArrowOutUpRight className="inline-flex w-4" />
+					</Link>{" "}
+				</p>
 			</div>
-		</div>
+		</>
 	);
 }
